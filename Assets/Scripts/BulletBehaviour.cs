5b073359194aedecefd030b6e3c1ef29d33e7df9
Michael-Naidoo <130663291+Michael-Naidoo@users.noreply.github.com>
2024-07-30 13:09:09 +0200

    basic shoot test

---
+++ Assets/Scripts/BulletBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class BulletBehaviour : MonoBehaviour
+    {
+        [SerializeField] private float speed;
+
+        private void Update()
+        {
+            Move();
+        }
+
+        void Move()
+        {
+            Vector3 target = transform.position + (transform.up * 10);
+            transform.position = Vector3.MoveTowards(transform.position, target, speed);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("CentiPiece"))
+            {
+                other.gameObject.GetComponent<CentipedeBehaviour>().HasBeenHit();
+                Destroy(gameObject);
+            }
+        }
+    }
+}
